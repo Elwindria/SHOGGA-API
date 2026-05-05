069e54533d97258b517cb62f49af5eebc5db3a5a
Elwindria <p.lopez2000@laposte.net>
2026-05-05 14:36:29 +0200

    rajout d'un mapper pour trouver les id des produit de catalogue en fonction d'un nom

---
+++ src/Service/Sellsy/Catalogue/SellsyCatalogueMappingResolver.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service\Sellsy\Catalogue;
+
+use App\Service\Sellsy\Catalogue\SellsyCatalogueService;
+
+final class SellsyCatalogueMappingResolver
+{
+    public function __construct(
+        private SellsyCatalogueService $sellsyCatalogueService
+    ) {
+    }
+
+    public function getCatalogueIdByName(string $name): int
+    {
+        $catalogueIds = $this->sellsyCatalogueService->getCatalogueIdsByName();
+
+        if (!isset($catalogueIds[$name])) {
+            throw new \RuntimeException(sprintf(
+                'Aucun produit Sellsy configuré pour le nom %s.',
+                $name
+            ));
+        }
+
+        return $catalogueIds[$name];
+    }
+}
