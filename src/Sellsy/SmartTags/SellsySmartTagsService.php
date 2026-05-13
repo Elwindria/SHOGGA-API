a82d8af84777fa2a1dda279ad8165bd581d7bbc3
Elwindria <p.lopez2000@laposte.net>
2026-05-13 14:56:38 +0200

    Changement de la request getSmartTag, l'API de Sellsy permet pas de récupérer tous les smartstag, mais seullement via un paramètre nom...

--- src/Sellsy/SmartTags/SellsySmartTagsService.php
+++ src/Sellsy/SmartTags/SellsySmartTagsService.php
@@ -19,35 +19,35 @@ final class SellsySmartTagsService
     ) {
     }
 
-    public function getSmartTags(): array
+    public function getSmartTagsByName(string $q): array
     {
-        return $this->cache->get(self::CACHE_KEY, function (ItemInterface $item) {
-            // durée de vie du cache (ex: 1 jour)
-            $item->expiresAfter(86400);
+        try {
+            $response = $this->sellsyV2Client->request(
+                'GET',
+                '/smart-tags/individual/autocomplete',
+                [
+                    'q' => $q,
+                ]
+            );
 
-            try {
-                $response = $this->sellsyV2Client->request('GET', '/smart-tags/individuals/autocomplete');
+            return $response['data'] ?? [];
+        } catch (\Throwable $e) {
+            $this->logger->error('Erreur récupération des SmartTags Sellsy V2', [
+                'q' => $q,
+                'error' => $e->getMessage(),
+            ]);
 
-                $this->logger->info('SmartTags Sellsy récupérées depuis API');
-
-                return $response;
-            } catch (\Throwable $e) {
-                $this->logger->error('Erreur récupération des SmartTags Sellsy V2', [
-                    'error' => $e->getMessage(),
-                ]);
-
-                throw $e;
-            }
-        });
+            throw $e;
+        }
     }
 
     //trouve l'id d'un smart tag en fonction de son noms (seulement pour les particulier ! Sellsy fait la différence des smarttags client/particulier/société etc)
     public function getSmartTagsIdOfIndividualsByName(string $name): ?int
     {
-        $tags = $this->getSmartTags() ?? [];
+        $tags = $this->getSmartTagsByName($name);
 
         foreach ($tags as $tag) {
-            if ($tag['value'] === $name) {
+            if (($tag['value'] ?? null) === $name) {
                 return $tag['id'];
             }
         }
