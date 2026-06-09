d3d2ac80f3053d2b43f8b7c4629527b89a6b5720
Elwindria <p.lopez2000@laposte.net>
2026-06-09 15:55:25 +0200

    Fix: Name Service

---
+++ src/GameContest/Service/GameContestRGPDCleanupService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\GameContest\Service;
+
+use App\Sellsy\Individual\SellsyIndividualService;
+use Psr\Log\LoggerInterface;
+use App\GameContest\Repository\GameContestEmailAttemptRepository;
+
+final class GameContestRGPDCleanupService
+{
+    public function __construct(
+        private readonly SellsyIndividualService $sellsyIndividualService,
+        private readonly LoggerInterface $logger,
+        private readonly GameContestEmailAttemptRepository $gameContestEmailAttemptRepository,
+    ) {
+    }
+
+    public function cleanExpiredIndividualsFromGameContest(): int
+    {
+        $count = 0;
+
+        $this->logger->info('[Sellsy][GameContest] Starting SHOGGA Game Contest RGPD cleanup');
+
+        $expiredIndividuals = $this->sellsyIndividualService->findExpiredIndividualsFromGameContest();
+
+        foreach ($expiredIndividuals['data'] ?? [] as $individual) {
+            $this->sellsyIndividualService->deleteIndividual($individual['id']);
+            $count++;
+
+            $this->logger->info('[Sellsy][GameContest] Deleted expired SHOGGA individual', [
+                'individual_id' => $individual['id'],
+                'email' => $individual['email'] ?? null,
+            ]);
+        }
+
+        $this->logger->info('[Sellsy][GameContest] SHOGGA Game Contest RGPD cleanup completed', [
+            'deleted_count' => $count,
+        ]);
+
+        return $count;
+    }
+
+    public function cleanEmailAttempt(): int
+    {
+        $deletedCount = $this->gameContestEmailAttemptRepository->deleteAll();
+
+        $this->logger->info('[GameContest] Temporary email attempts cleaned', [
+            'deleted_count' => $deletedCount,
+        ]);
+
+        return $deletedCount;
+    }
+    
+}
